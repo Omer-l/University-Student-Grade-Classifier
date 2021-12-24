da28b0b7e7eb21caff3eef823216fc7ac708e013
Omer Kacar <omerkacar1@gmail.com>
2021-12-24 23:59:04 +0000

    feat(core): add one less grade Profile

--- src/Degree.java
+++ src/Degree.java
@@ -4,11 +4,18 @@ public class Degree {
 	// Your additions/changes below this line
 	private final int numberOfGradesYear2 = 4;
 	private final int numberOfGradesYear3 = 4;
+	private final Profile level5Profile;
+	private final Profile level6Profile;
 
 	public Degree(List<Grade> year2, List<Grade> year3) {
 		if (year2 == null || year3 == null || year2.size() != numberOfGradesYear2 || year3.size() != numberOfGradesYear3
 				|| fail(year2, year3))
 			throw new IllegalArgumentException();
+		else {
+			year2.addAll(year3);
+			level5Profile = new Profile(year2);
+			level6Profile = new Profile(year3);
+		}
 	}
 
 	/**
@@ -32,6 +39,17 @@ public class Degree {
 	}
 
 	public Classification classify() {
+		Integer level5Classification = level5Profile.classify().ordinal();
+		Integer level6Classification = level6Profile.classify().ordinal();
+		Classification[] classificationValues = Classification.values();
+		
+		if(level5Classification.equals(level6Classification))
+			return classificationValues[level5Classification];
+		else if(level5Classification == level6Classification - 1) //level 5 is 1 higher
+			return classificationValues[level5Classification];
+		else if(level6Classification == level5Classification - 1) //level 6 is 1 higher
+			return classificationValues[level6Classification];
+		
 		return Classification.Third;
 	}
 }
--- src/DegreeTest.java
+++ src/DegreeTest.java
@@ -60,4 +60,50 @@ class DegreeTest {
 			Degree Degree = new Degree(year2Grades, year3Grades);
 		});
 	}
+	
+	@Test
+	void testClassifyBothProfilesSame() {
+		List<Grade> year2Grades = new ArrayList<>();
+		year2Grades.add(new Grade(1)); //grade 1
+		year2Grades.add(new Grade(1)); //grade 2
+		year2Grades.add(new Grade(1)); //grade 3
+		year2Grades.add(new Grade(1)); //grade 4
+		
+		
+		List<Grade> year3Grades = new ArrayList<>();
+		year3Grades.add(new Grade(1)); //grade 1
+		year3Grades.add(new Grade(1)); //grade 2
+		year3Grades.add(new Grade(14)); //grade 3
+		year3Grades.add(new Grade(1)); //grade 4
+		
+		Degree degree = new Degree(year2Grades, year3Grades);
+		
+		Classification expected = Classification.First;
+		Classification actual = degree.classify();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testClassifyLevel5Higher() {
+		List<Grade> year2Grades = new ArrayList<>(); //Second
+		year2Grades.add(new Grade(5)); //grade 1
+		year2Grades.add(new Grade(8)); //grade 2
+		year2Grades.add(new Grade(7)); //grade 3
+		year2Grades.add(new Grade(6)); //grade 4
+		
+		
+		List<Grade> year3Grades = new ArrayList<>(); //First
+		year3Grades.add(new Grade(1)); //grade 1
+		year3Grades.add(new Grade(1)); //grade 2
+		year3Grades.add(new Grade(14)); //grade 3
+		year3Grades.add(new Grade(1)); //grade 4
+		
+		Degree degree = new Degree(year2Grades, year3Grades);
+		
+		Classification expected = Classification.First;
+		Classification actual = degree.classify();
+		
+		assertEquals(expected, actual);
+	}
 }
