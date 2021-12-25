635c20828dda0a85db366f6a4deb960523c86f2b
Omer Kacar <omerkacar1@gmail.com>
2021-12-25 00:46:05 +0000

    fix(core): fix degree classification
    The lower grade is being classified instead of the higher grade
    for the degree.

--- src/Degree.java
+++ src/Degree.java
@@ -45,10 +45,10 @@ public class Degree {
 		
 		if(level5Classification.equals(level6Classification))
 			return classificationValues[level5Classification];
-		else if(level5Classification == level6Classification - 1) //level 5 is 1 higher
-			return classificationValues[level5Classification];
-		else if(level6Classification == level5Classification - 1) //level 6 is 1 higher
+		else if(level5Classification == level6Classification - 1) //level 6 is 1 higher
 			return classificationValues[level6Classification];
+		else if(level6Classification == level5Classification - 1) //level 5 is 1 higher
+			return classificationValues[level5Classification];
 		
 		return Classification.Third;
 	}
